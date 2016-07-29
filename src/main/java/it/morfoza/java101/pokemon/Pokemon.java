90a916c27f2bda2f689257925c809301dbfb72a5
Karola <yumi.chan@interia.pl>
2016-07-29 15:13:24 +0200

    variables

---
+++ src/main/java/it/morfoza/java101/pokemon/Pokemon.java
@@ -0,0 +1,12 @@
+package it.morfoza.java101.pokemon;
+
+
+public class Pokemon {
+    public static void main(String[] args) {
+        Pokemon pokemon = new Pokemon();
+
+        String pokemonDescription = pokemon.toString();
+
+        System.out.println(pokemonDescription);
+    }
+}
